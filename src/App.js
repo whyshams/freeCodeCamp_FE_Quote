7d258cba24976532f936f8544248e5f3cbba4718
Nuren Shams Chowdhury <chowdhury.noren@gmail.com>
2022-10-01 21:08:53 +0600

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useEffect,useState} from 'react';
+import axios from 'axios';
+import './index.css'
 
-function App() {
+const App = () => {
+    const [quoteData,setQouteData] = useState([]);
+    const [newQoute,setNewQuote] = useState(false)
+    useEffect(() => {
+        axios.request({
+            method: 'GET',
+            url: 'https://famous-quotes4.p.rapidapi.com/random',
+            params: {category: 'all', count: '1'},
+            headers: {
+              'X-RapidAPI-Key': '42ca516b7amshb3430e4d7c91715p1b5024jsnb40e2e325bd9',
+              'X-RapidAPI-Host': 'famous-quotes4.p.rapidapi.com'
+            }
+        }).then(function (response) {
+            console.log(response.data);
+            setQouteData(response.data)
+        }).catch(function (error) {
+            console.error(error);
+        });
+    },[newQoute])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+     <div>
+        {
+            quoteData.map((data) =>(
+                <div key={data.author} id="quote-box" >
+                <div  className=' p-3 m-3 d-flex justify-content-center align-items-center' id="text">
+                    <h3>
+                   <q> {data.text}</q>
+                    </h3>
+                </div>
+                <div id="author">
+                    <h1 className=' p-3 m-3 d-flex justify-content-center align-items-center'>
+                    {data.author}
+                    </h1>
+                </div>
+                <div >
+                    <button className='btn btn-danger p-3 m-3 d-flex justify-content-center align-items-center' id="new-quote" onClick={() => setNewQuote(!newQoute)}>
+                        New Quote
+                    </button>
+                </div>
+                <div>
+                    <a className='btn btn-primary p-3 m-3 d-flex justify-content-center align-items-center' id="tweet-quote" rel='noreferrer' target="_blank" href="https://www.twitter.com/intent/tweet">Share in Twitter</a>
+                </div>    
+                </div>
+            ))
+        }
+
     </div>
-  );
+    </>
+   
+  )
 }
 
-export default App;
+export default App
